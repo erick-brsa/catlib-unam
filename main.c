4acf26aee331e443849611f862a7ed7a2e12261d
Erick Briones <erickalan518@gmail.com>
2023-11-26 11:28:32 -0600

    Finalizando el proyecto

--- main.c
+++ main.c
@@ -16,6 +16,12 @@ struct Libro {
     int cantidad;
 };
 
+struct Registro {
+    int numero_cuenta;
+    int clave;
+    char fecha[11];
+};
+
 int validar(struct Cuenta *usuario);
 
 void obtenerLibros(struct Libro *libros);
@@ -30,11 +36,13 @@ void actualizarLibros(struct Libro *libros, int maxLibros, int clave);
 
 void crearRegistro(struct Cuenta *usuario, struct Libro *libro);
 
+void actualizarRegistros(struct Cuenta *usuario, struct Libro *libro);
+
 void solicitarLibro(struct Cuenta *usuario, struct Libro *libros, int maxLibros);
 
-void devolverLibro(struct Cuenta *usuario);
+void devolverLibro(struct Cuenta *usuario, struct Libro *libros, int maxLibros);
 
-void pagarAdeudos();
+void pagarAdeudos(struct Cuenta *usuario);
 
 int main() {
     struct Cuenta usuario;
@@ -62,10 +70,10 @@ int main() {
                 solicitarLibro(&usuario, libros, 20);
                 break;
             case 3:
-                devolverLibro(&usuario);
+                devolverLibro(&usuario, libros, 20);
                 break;
             case 4:
-                pagarAdeudos();
+                pagarAdeudos(&usuario);
                 break;
             case 5:
                 printf("Bye.\n");
@@ -137,8 +145,8 @@ void obtenerLibros(struct Libro *libros) {
 
 void mostrarMenu() {
     printf("1. Mostrar libros.\n");
-    printf("2. Solicitar Libro(s).\n");
-    printf("3. Devolver Libro(s).\n");
+    printf("2. Solicitar libro(s).\n");
+    printf("3. Devolver libro(s).\n");
     printf("4. Pagar adeudos.\n");
     printf("5. Salir.\n");
 }
@@ -175,6 +183,40 @@ void crearRegistro(struct Cuenta *usuario, struct Libro *libro) {
     fclose(archivo);
 }
 
+void actualizarRegistros(struct Cuenta *usuario, struct Libro *libro) {
+    FILE *archivo_lectura = fopen("records.txt", "r");
+    FILE *archivo_temp = fopen("temp_records.txt", "w");
+
+    printf("%d %d %s\n", libro->clave, libro->cantidad, libro->nombre);
+    printf("%d \n", usuario->numero_cuenta);
+
+    if (archivo_lectura == NULL || archivo_temp == NULL) {
+        printf("Error al abrir archivos.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct Registro registro;
+    int encontrado = 0;
+
+    while (fscanf(archivo_lectura, "%d,%d,%s", &registro.numero_cuenta, &registro.clave, registro.fecha) == 3) {
+        if (registro.numero_cuenta == usuario->numero_cuenta && registro.clave == libro->clave) {
+            encontrado = 1;
+            continue;
+        }
+        fprintf(archivo_temp, "%d,%d,%s\n", registro.numero_cuenta, registro.clave, registro.fecha);
+    }
+
+    fclose(archivo_temp);
+    fclose(archivo_lectura);
+
+    if (encontrado) {
+        remove("records.txt");
+        rename("temp_records.txt", "records.txt");
+    } else {
+        remove("temp_records.txt");
+    }
+}
+
 void actualizarUsuarios(struct Cuenta *usuario) {
     FILE *archivo = fopen("users.txt", "r"); // Abre en modo lectura
 
@@ -199,12 +241,14 @@ void actualizarUsuarios(struct Cuenta *usuario) {
 
         if (sscanf(linea, "%d,%f,%d", &cuentaArchivo, &adeudosArchivo, &cantidadLibrosArchivo) == 3 &&
             cuentaArchivo == usuario->numero_cuenta) {
-            // Actualiza la cantidad de libros y agrega el nuevo código al final
-            fprintf(tempArchivo, "%d,%.1f,%d", cuentaArchivo, adeudosArchivo, cantidadLibrosArchivo + 1);
-            for (int i = 0; i < cantidadLibrosArchivo; i++) {
-                fprintf(tempArchivo, ",%d", usuario->libros_asociados[i]);
+            // Actualiza la cantidad de libros
+            fprintf(tempArchivo, "%d,%.1f,%d", usuario->numero_cuenta, usuario->adeudos, usuario->cantidad_libros);
+            if (usuario->cantidad_libros > 0) {
+                for (int i = 0; i < usuario->cantidad_libros; i++) {
+                    fprintf(tempArchivo, ",%d", usuario->libros_asociados[i]);
+                }
             }
-            fprintf(tempArchivo, ",%d\n", usuario->libros_asociados[cantidadLibrosArchivo]);
+            fprintf(tempArchivo, "\n");
         } else {
             // Copia la línea sin cambios al archivo temporal
             fprintf(tempArchivo, "%s", linea);
@@ -250,7 +294,7 @@ void actualizarLibros(struct Libro *libros, int maxLibros, int clave) {
 }
 
 void solicitarLibro(struct Cuenta *usuario, struct Libro *libros, int maxLibros) {
-    printf("En este momento cuentas con %d Libro(s).\n", usuario->cantidad_libros);
+    printf("En este momento cuentas con %d libro(s).\n", usuario->cantidad_libros);
 
     if (usuario->cantidad_libros >= 20) {
         printf("Ya no puedes solicitar m%cs libros.\n", 160);
@@ -288,7 +332,7 @@ void solicitarLibro(struct Cuenta *usuario, struct Libro *libros, int maxLibros)
     printf("No se encontr%c el libro.\n", 162);
 }
 
-void devolverLibro(struct Cuenta *usuario) {
+void devolverLibro(struct Cuenta *usuario, struct Libro *libros, int maxLibros) {
     if (usuario->cantidad_libros == 0) {
         printf("No hay libros asociados a tu cuenta.\n");
         return;
@@ -300,12 +344,113 @@ void devolverLibro(struct Cuenta *usuario) {
     }
 
     int clave;
-    printf("\nIngrese el c%cdigo del Libro que desea devolver:%c", 162, 255);
+    printf("\nIngrese el c%cdigo del libro que desea devolver:%c", 162, 255);
     scanf("%d", &clave);
 
-    // TODO: Agregar la funcionalidad
+    int indice = -1;
+
+    for (int i = 0; i < usuario->cantidad_libros; i++) {
+        if (usuario->libros_asociados[i] == clave) {
+            indice = i;
+        }
+    }
+
+    if (indice == -1) {
+        printf("El libro no est%c asociado a tu cuenta.\n", 160);
+        return;
+    }
+
+    struct Registro registro;
+
+    FILE *archivo = fopen("records.txt", "r");
+
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo records.txt\n");
+        exit(1);
+    }
+
+    struct tm fecha_tm = {0};
+    time_t fecha_archivo, fecha_actual;
+
+    while (fscanf(archivo, "%d,%d,%10s", &registro.numero_cuenta, &registro.clave, registro.fecha) == 3) {
+        if (registro.numero_cuenta == usuario->numero_cuenta && registro.clave == clave) {
+            if (sscanf(registro.fecha, "%d/%d/%d", &fecha_tm.tm_mday, &fecha_tm.tm_mon, &fecha_tm.tm_year) != 3) {
+                printf("Error al convertir la fecha.\n");
+                exit(1);
+            }
+            fecha_tm.tm_year -= 1900;
+            fecha_tm.tm_mon -= 1;
+
+            time(&fecha_actual);
+            fecha_archivo = mktime(&fecha_tm);
+
+            if (difftime(fecha_actual, fecha_archivo) > 1) {
+                int dias_atraso = difftime(fecha_actual, fecha_archivo) / (24 * 60 * 60);
+                float deuda = dias_atraso * 2.5;
+                usuario->adeudos += (float) deuda;
+                printf("D%cas de atraso: %d\n", 161, dias_atraso);
+                printf("Deber%cs pagar una deuda de: %.2f\n", 160, deuda);
+            } else {
+                printf("Entregado a tiempo.\n");
+            }
+        }
+    }
+
+    fclose(archivo);
+
+    for (int i = indice; i < usuario->cantidad_libros - 1; i++) {
+        usuario->libros_asociados[i] = usuario->libros_asociados[i + 1];
+    }
+
+    for (int i = 0; i < maxLibros; i++) {
+        if (libros[i].clave == clave) {
+            indice = i;
+            libros[i].cantidad += 1;
+        }
+    }
+
+    usuario->cantidad_libros -= 1;
+
+    actualizarUsuarios(usuario);
+    actualizarLibros(libros, maxLibros, clave);
+    actualizarRegistros(usuario, &libros[indice]);
+
 }
 
-void pagarAdeudos() {
-    // TODO: Agregar la funcionalidad
+void pagarAdeudos(struct Cuenta *usuario) {
+    if (usuario->adeudos == 0) {
+        printf("No tienes adeudos.\n");
+        return;
+    }
+    printf("Debes %.2f\n", usuario->adeudos);
+
+    char option;
+    printf("Deseas realizar alg%cn pago? (y/n):%c", 163, 255);
+    scanf(" %c", &option);
+
+    if (option == 'y' || option == 'Y') {
+        int abono;
+        printf("Ingrese la cantidad de dinero que desea abonar:%c", 255);
+        scanf("%d", &abono);
+
+        usuario->adeudos -= abono;
+
+        printf("Deseas abonar m%cs? (y/n):%c", 160, 255);
+        scanf(" %c", &option);
+
+        // Mientras el usuario quiera abonar más
+        while (option == 'y' || option == 'Y') {
+            printf("Ingrese la cantidad de dinero que desea abonar: ");
+            scanf("%d", &abono);
+
+            // Restar el abono a los adeudos
+            usuario->adeudos -= abono;
+
+            printf("Deseas abonar m%cs? (y/n):%c", 160, 255);
+            scanf(" %c", &option);
+        }
+    }
+
+    printf("La deuda final es: %.2f\n", usuario->adeudos);
+    actualizarUsuarios(usuario);
 }
